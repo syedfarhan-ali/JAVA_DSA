f2b8b5aba89328a814ab161c2e3644e37b7836fb
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-06-16 15:44:59 +0530

    added type casting

---
+++ TypeCasting.java
@@ -0,0 +1,12 @@
+class TypeCasting{
+    public static void main(String args[]){
+        float marks = 99.9f;
+        int marksInt = (int)marks;
+        System.out.println(marksInt);
+
+        int number = 65;
+        char ch = (char)number;
+        System.out.println(ch);
+
+    }
+}
