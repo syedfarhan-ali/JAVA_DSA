f510393ac3dcbbd82b9e2a175d674acac0715257
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-06-16 15:43:30 +0530

    added Java basics  program

---
+++ JavaBasics.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class JavaBasics{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        //next() for single word input
+        // String input = sc.next();
+        // System.out.print(input);
+
+        //nextLine() for multi word/paragraph input
+        String name = sc.nextLine();
+        System.out.println(name);
+
+        //nextInt() for integer input
+        // int number = sc.nextInt();
+        // System.out.println(number);
+
+        //nextFloat() for float input
+        // float price = sc.nextFloat();
+        // System.out.println(price);
+
+        // Boolean pass = sc.nextBoolean();
+        // System.out.println(pass);
+
+        // short num = sc.nextShort();
+        // System.out.println(num);
+    }
+}
