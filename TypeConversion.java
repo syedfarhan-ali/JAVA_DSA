9340e89bcc5dd9f24df0de45a0923a3e135ac195
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-06-16 15:45:41 +0530

    added type convertion

---
+++ TypeConversion.java
@@ -0,0 +1,8 @@
+class TypeConversion{
+    public static void main(String args[]){
+        // byte->short->int->float->long->double
+        int a = 25;
+        long b = a;
+        System.out.println(b);
+    }
+}
