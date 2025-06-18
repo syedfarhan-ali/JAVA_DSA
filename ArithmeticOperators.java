956d0db6653d14491db8754288db835c688d2c84
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-06-18 20:45:12 +0530

    added operators

---
+++ ArithmeticOperators.java
@@ -0,0 +1,29 @@
+class ArithmeticOperators{
+    public static void main(String args[]){
+        int a = 10,b = 5;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(++a);
+        System.out.println(++b);
+        System.out.println(--a);
+        System.out.println(--b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(true && false);
+        System.out.println(true || false);
+        System.out.println(!true);
+        System.out.println(a = b);
+        System.out.println(a += b);
+        System.out.println(a -= b);
+        System.out.println(a *= b);
+        System.out.println(a /= b);
+
+    }
+}
