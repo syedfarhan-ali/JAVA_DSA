5e47c056e42e1ee936f64a4ee6a8c66e94ebdccc
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-07-31 19:59:46 +0530

    added some hashing codes

---
+++ Hashing/HashMapOperations.java
@@ -0,0 +1,39 @@
+package Hashing;
+import java.util.HashMap;
+
+public class HashMapOperations {
+    public static void main(String[] args) {
+        //Create
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        //Insert - O(1)
+        hm.put("India", 100);
+        hm.put("China", 150);
+        hm.put("US", 50);
+
+        System.out.println(hm);
+
+        //Get - O(1)
+        int population = hm.get("India");
+        System.out.println(population);
+
+        System.out.println(hm.get("Indonesia"));//null
+
+        //ContainsKey - O(1)
+        System.out.println(hm.containsKey("India"));//true
+        System.out.println(hm.containsKey("Indonesia"));//false
+
+        //Remove - O(1)
+        System.out.println(hm.remove("China"));
+        System.out.println(hm);
+
+        //Size
+        System.out.println(hm.size());
+
+        //Clear
+        hm.clear();
+        
+        //IsEmpty
+        System.out.println(hm.isEmpty());
+    }
+}
