dc2a7e6d5aaa384377ad9a592d30a3cb48af136a
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-08-14 20:12:26 +0530

    added bellmanfords and prims algo

---
+++ Graphs_Part3/BellmanFord.java
@@ -0,0 +1,119 @@
+package Graphs_Part3;
+
+import java.util.ArrayList;
+
+public class BellmanFord {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+        public Edge(int src, int dest, int wt){
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]){
+        for(int i=0; i<graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
+
+        graph[1].add(new Edge(1, 2, -4));
+
+        graph[2].add(new Edge(2, 3, 2));
+
+        graph[3].add(new Edge(3, 4, 4));
+
+        graph[4].add(new Edge(4, 1, -1));
+    }
+
+    static void createGraph2(ArrayList<Edge> graph){
+        graph.add(new Edge(0, 1, 2));
+        graph.add(new Edge(0, 2, 4));
+        graph.add(new Edge(1, 2, -4));
+        graph.add(new Edge(2, 3, 2));
+        graph.add(new Edge(3, 4, 4));
+        graph.add(new Edge(4, 1, -1));
+    }
+
+    public static void bellmanFord(ArrayList<Edge> graph[], int src){
+        int dist[] = new int[graph.length];
+        for(int i=0; i<dist.length; i++){
+            if(i != src){
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+        int vertices = graph.length;
+        //O(V*E)
+        //algo - O(V)
+        for(int i=0; i<vertices - 1; i++){
+            //edges - O(E)
+            for(int j=0; j<graph.length; j++){
+                for(int k=0; k<graph[j].size(); k++){
+                    Edge e = graph[j].get(k);
+
+                    int u = e.src; //we don't actully have to make these
+                    int v = e.dest; //but im doing it for clarity anyway
+                    int wt = e.wt; //we can simply use e.src, e.dest and e.wt directly
+
+                    //relaxation
+                    if (dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]) {
+                        dist[v] = dist[u] + wt;
+                    }
+
+                }
+            }
+        }
+        //print 
+        for(int i=0; i<dist.length; i++){
+            System.out.print(dist[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void bellmanFord2(ArrayList<Edge> graph, int src, int vertices){
+        int dist[] = new int[vertices];
+        for(int i=0; i<dist.length; i++){
+            if(i != src){
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+        
+        // O(V*E)
+        for(int i=0; i<vertices - 1; i++){
+            //edges - O(E)
+            for(int j=0; j<graph.size(); j++){
+                    Edge e = graph.get(j);
+
+                    int u = e.src; //we don't actully have to make these
+                    int v = e.dest; //but im doing it for clarity anyway
+                    int wt = e.wt; //we can simply use e.src, e.dest and e.wt directly
+
+                    //relaxation
+                    if (dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]) {
+                        dist[v] = dist[u] + wt;
+                    }
+
+                }
+        }
+        //print 
+        for(int i=0; i<dist.length; i++){
+            System.out.print(dist[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        int vertices = 5;
+        // ArrayList<Edge> graph[] = new ArrayList[vertices];
+        // createGraph(graph);
+        // bellmanFord(graph, 0);
+        ArrayList<Edge> graph = new ArrayList<>();
+        createGraph2(graph);
+        bellmanFord2(graph, 0, vertices);
+    }
+}
---
+++ Graphs_Part3/Prims.java
@@ -0,0 +1,78 @@
+package Graphs_Part3;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class Prims {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+        public Edge(int src, int dest, int wt){
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge>[] graph){
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 10));
+        graph[0].add(new Edge(0, 2, 15));
+        graph[0].add(new Edge(0, 3, 30));
+
+        graph[1].add(new Edge(1, 0, 10));
+        graph[1].add(new Edge(1, 3, 40));
+        
+        graph[2].add(new Edge(2, 0, 15));
+        graph[2].add(new Edge(2, 3, 50));
+
+        graph[3].add(new Edge(3, 1, 40));
+        graph[3].add(new Edge(3, 2, 50));
+    }
+
+    static class Pair implements Comparable<Pair> {
+        int v;
+        int cost;
+        public Pair(int v, int cost){
+            this.v = v;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Pair p2){
+            return this.cost - p2.cost; //ascending
+        }
+    }
+
+    public static void prims(ArrayList<Edge>[] graph){
+        boolean[] visited = new boolean[graph.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(0, 0));
+        int finalCost = 0; //MST cost or Total min weight
+
+        while (!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if (!visited[curr.v]) {
+                visited[curr.v] = true;
+                finalCost += curr.cost;
+
+                for(int i=0; i<graph[curr.v].size(); i++){
+                    Edge e = graph[curr.v].get(i);
+                    pq.add(new Pair(e.dest, e.wt));
+                }
+            }
+        }
+        System.out.println("Final minimum cost = "+finalCost);
+    }
+
+    public static void main(String[] args) {
+        int vertices = 4;
+        ArrayList<Edge>[] graph = new ArrayList[vertices];
+        createGraph(graph);
+        prims(graph);
+    }
+}
